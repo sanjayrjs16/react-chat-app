9dae573dcc10d36d106df88ae847ecbcd23f52a3
Sanjay <sanjayrjs16@gmail.com>
2021-08-02 08:18:39 +0530

    Chat app boiler plate

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom' 
+
+import Join from './components/Join';
+import Chat from './components/Chat';
+
+function App() {
+  return (
+   <Router>
+     <Route path="/" exact  component={Join} />
+     <Route path="/chat" component={Chat} />
+   </Router>
+  );
+}
+
+export default App;
